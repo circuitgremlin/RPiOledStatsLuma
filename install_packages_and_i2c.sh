4db606b33b623f03970f027a4f4a48eeedce62e7
Benjamin Lenherr <sunsetroad@gmx.ch>
2026-06-13 19:06:11 +0200

    Update install_packages_and_i2c.sh

--- install_packages_and_i2c.sh
+++ install_packages_and_i2c.sh
@@ -6,15 +6,11 @@ sudo apt update && sudo apt -y full-upgrade
 
 # Install apt packages
 echo "Install apt packages"
-sudo apt install -y python3 python3-pip i2c-tools git
+sudo apt install -y i2c-tools git
 
 # Enable I2C interface
 echo "Enable I2C interface"
 sudo raspi-config nonint do_i2c 0
-
-# Check the I2C status
-echo "Check the I2C status"
-sudo i2cdetect -y 1
 
 # Reboot system
 echo "Reboot system"
